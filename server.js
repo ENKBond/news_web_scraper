34767eaa71b07fed75dc46738141aeff1dcb4366
ENK Bond <43937176+ENKBond@users.noreply.github.com>
2019-03-25 18:10:10 -0700

    final

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.set("view engine", "handlebars");
 
 
 // Connect to the Mongo DB
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI);
 
 //routes
